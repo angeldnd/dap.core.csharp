d44e27c5b19b2bc4e092702153b031a45ff51452
YJ Park <yjpark@gmail.com>
2015-07-12 18:19:58 +0800

    some debug log

--- Scripts/DapCore/core_/Aspect.cs
+++ Scripts/DapCore/core_/Aspect.cs
@@ -50,37 +50,47 @@ namespace angeldnd.dap {
         }                                                             //__SILP__
                                                                       //__SILP__
         //SILP: ASPECT_MIXIN()
-        private Entity _Entity = null;                                       //__SILP__
-        public Entity Entity {                                               //__SILP__
-            get { return _Entity; }                                          //__SILP__
-        }                                                                    //__SILP__
-                                                                             //__SILP__
-        private string _Path = null;                                         //__SILP__
-        public string Path {                                                 //__SILP__
-            get { return _Path; }                                            //__SILP__
-        }                                                                    //__SILP__
-                                                                             //__SILP__
-        public string RevPath {                                              //__SILP__
-            get {                                                            //__SILP__
-                return string.Format("{0} ({1})", _Path, Revision);          //__SILP__
-            }                                                                //__SILP__
-        }                                                                    //__SILP__
-                                                                             //__SILP__
-        private bool _Inited = false;                                        //__SILP__
-        public bool Inited {                                                 //__SILP__
-            get { return _Inited; }                                          //__SILP__
-        }                                                                    //__SILP__
-                                                                             //__SILP__
-        public bool Init(Entity entity, string path) {                       //__SILP__
-            if (_Inited) return false;                                       //__SILP__
-            if (entity == null || string.IsNullOrEmpty(path)) return false;  //__SILP__
-                                                                             //__SILP__
-            _Entity = entity;                                                //__SILP__
-            _Path = path;                                                    //__SILP__
-            _Inited = true;                                                  //__SILP__
-            return true;                                                     //__SILP__
-        }                                                                    //__SILP__
-                                                                             //__SILP__
+        private Entity _Entity = null;                                //__SILP__
+        public Entity Entity {                                        //__SILP__
+            get { return _Entity; }                                   //__SILP__
+        }                                                             //__SILP__
+                                                                      //__SILP__
+        private string _Path = null;                                  //__SILP__
+        public string Path {                                          //__SILP__
+            get { return _Path; }                                     //__SILP__
+        }                                                             //__SILP__
+                                                                      //__SILP__
+        public string RevPath {                                       //__SILP__
+            get {                                                     //__SILP__
+                return string.Format("{0} ({1})", _Path, Revision);   //__SILP__
+            }                                                         //__SILP__
+        }                                                             //__SILP__
+                                                                      //__SILP__
+        private bool _Inited = false;                                 //__SILP__
+        public bool Inited {                                          //__SILP__
+            get { return _Inited; }                                   //__SILP__
+        }                                                             //__SILP__
+                                                                      //__SILP__
+        public bool Init(Entity entity, string path) {                //__SILP__
+            if (_Inited) {                                            //__SILP__
+                Error("Already Inited: {0}, {1}", entity, path);      //__SILP__
+                return false;                                         //__SILP__
+            }                                                         //__SILP__
+            if (entity == null) {                                     //__SILP__
+                Error("Invalid Entity: {0}, {1}", entity, path);      //__SILP__
+                return false;                                         //__SILP__
+            }                                                         //__SILP__
+            if (string.IsNullOrEmpty(path)) {                         //__SILP__
+                Error("Invalid Path: {0}, {1}", entity, path);        //__SILP__
+                return false;                                         //__SILP__
+            }                                                         //__SILP__
+                                                                      //__SILP__
+            _Entity = entity;                                         //__SILP__
+            _Path = path;                                             //__SILP__
+            _Inited = true;                                           //__SILP__
+            return true;                                              //__SILP__
+        }                                                             //__SILP__
+                                                                      //__SILP__
         //SILP: ASPECT_EVENTS_MIXIN()
         public virtual void OnAdded() {}                              //__SILP__
         public virtual void OnRemoved() {}                            //__SILP__
@@ -183,37 +193,47 @@ namespace angeldnd.dap {
         }
 
         //SILP: ASPECT_MIXIN()
-        private Entity _Entity = null;                                       //__SILP__
-        public Entity Entity {                                               //__SILP__
-            get { return _Entity; }                                          //__SILP__
-        }                                                                    //__SILP__
-                                                                             //__SILP__
-        private string _Path = null;                                         //__SILP__
-        public string Path {                                                 //__SILP__
-            get { return _Path; }                                            //__SILP__
-        }                                                                    //__SILP__
-                                                                             //__SILP__
-        public string RevPath {                                              //__SILP__
-            get {                                                            //__SILP__
-                return string.Format("{0} ({1})", _Path, Revision);          //__SILP__
-            }                                                                //__SILP__
-        }                                                                    //__SILP__
-                                                                             //__SILP__
-        private bool _Inited = false;                                        //__SILP__
-        public bool Inited {                                                 //__SILP__
-            get { return _Inited; }                                          //__SILP__
-        }                                                                    //__SILP__
-                                                                             //__SILP__
-        public bool Init(Entity entity, string path) {                       //__SILP__
-            if (_Inited) return false;                                       //__SILP__
-            if (entity == null || string.IsNullOrEmpty(path)) return false;  //__SILP__
-                                                                             //__SILP__
-            _Entity = entity;                                                //__SILP__
-            _Path = path;                                                    //__SILP__
-            _Inited = true;                                                  //__SILP__
-            return true;                                                     //__SILP__
-        }                                                                    //__SILP__
-                                                                             //__SILP__
+        private Entity _Entity = null;                                //__SILP__
+        public Entity Entity {                                        //__SILP__
+            get { return _Entity; }                                   //__SILP__
+        }                                                             //__SILP__
+                                                                      //__SILP__
+        private string _Path = null;                                  //__SILP__
+        public string Path {                                          //__SILP__
+            get { return _Path; }                                     //__SILP__
+        }                                                             //__SILP__
+                                                                      //__SILP__
+        public string RevPath {                                       //__SILP__
+            get {                                                     //__SILP__
+                return string.Format("{0} ({1})", _Path, Revision);   //__SILP__
+            }                                                         //__SILP__
+        }                                                             //__SILP__
+                                                                      //__SILP__
+        private bool _Inited = false;                                 //__SILP__
+        public bool Inited {                                          //__SILP__
+            get { return _Inited; }                                   //__SILP__
+        }                                                             //__SILP__
+                                                                      //__SILP__
+        public bool Init(Entity entity, string path) {                //__SILP__
+            if (_Inited) {                                            //__SILP__
+                Error("Already Inited: {0}, {1}", entity, path);      //__SILP__
+                return false;                                         //__SILP__
+            }                                                         //__SILP__
+            if (entity == null) {                                     //__SILP__
+                Error("Invalid Entity: {0}, {1}", entity, path);      //__SILP__
+                return false;                                         //__SILP__
+            }                                                         //__SILP__
+            if (string.IsNullOrEmpty(path)) {                         //__SILP__
+                Error("Invalid Path: {0}, {1}", entity, path);        //__SILP__
+                return false;                                         //__SILP__
+            }                                                         //__SILP__
+                                                                      //__SILP__
+            _Entity = entity;                                         //__SILP__
+            _Path = path;                                             //__SILP__
+            _Inited = true;                                           //__SILP__
+            return true;                                              //__SILP__
+        }                                                             //__SILP__
+                                                                      //__SILP__
         //SILP: ASPECT_LOG_MIXIN()
         public virtual string GetLogPrefix() {                                                             //__SILP__
             if (_Entity != null) {                                                                         //__SILP__
