784b815cb32ea2652c88364b10eec38b08260db5
YJ Park <yjpark@gmail.com>
2016-03-09 16:57:50 +0800

    Cleanup UriConsts

--- Scripts/DapCore/uri_/UriConsts.cs
+++ Scripts/DapCore/uri_/UriConsts.cs
@@ -3,28 +3,12 @@ using System.Collections.Generic;
 
 namespace angeldnd.dap {
     public static class UriConsts {
-        public const char UriSeparator = ':';
+        public const char PathSeparator = ':';
 
-        public const string UriSeparatorAsString = ":";
+        public const string PathSeparatorAsString = ":";
 
-        public static string Encode(string path, string param) {
-            return string.Format("{0}{1}{2}", path, UriSeparator, param);
-        }
-
-        public static void Decode(string uri, out string path, out string param) {
-            if (!string.IsNullOrEmpty(uri)) {
-                int pos = uri.IndexOf(UriSeparator);
-                if (pos < 0) {
-                    path = uri;
-                    param = null;
-                } else {
-                    path = uri.Remove(pos);
-                    param = uri.Substring(pos + 1);
-                }
-            } else {
-                path = null;
-                param = null;
-            }
+        public static string Encode(string pathA, string pathB) {
+            return string.Format("{0}{1}{2}", pathA, PathSeparator, pathB);
         }
     }
 }
