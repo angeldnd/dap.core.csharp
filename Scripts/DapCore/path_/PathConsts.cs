87096b9887e134042e2d915329d30eaa9f062d7a
YJ Park <yjpark@gmail.com>
2016-04-21 12:58:57 +0800

    minor tweak on API

--- Scripts/DapCore/path_/PathConsts.cs
+++ Scripts/DapCore/path_/PathConsts.cs
@@ -8,15 +8,21 @@ namespace angeldnd.dap {
         public const string PathSeparatorAsString = "/";
 
         public static string PathToKey(string path) {
+            if (path == null) return string.Empty;
+
             return path.Replace(PathSeparator, DictConsts.KeySeparator);
         }
 
         public static string KeyToPath(string key) {
+            if (key == null) return string.Empty;
+
             return key.Replace(DictConsts.KeySeparator, PathSeparator);
         }
 
         public static string Encode(string path, string relPath) {
             if (string.IsNullOrEmpty(path)) {
+                if (relPath == null) return string.Empty;
+
                 return relPath;
             } else {
                 return string.Format("{0}{1}{2}", path, PathSeparator, relPath);
