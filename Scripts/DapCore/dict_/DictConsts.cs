110a4575a275913516c63a5dee8193a15d699b4e
YJ Park <yjpark@gmail.com>
2016-06-14 14:30:31 +0800

    DictConsts helpers

--- Scripts/DapCore/dict_/DictConsts.cs
+++ Scripts/DapCore/dict_/DictConsts.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace angeldnd.dap {
     public static class DictConsts {
@@ -7,9 +8,19 @@ namespace angeldnd.dap {
 
         public const string KeySeparatorAsString = ".";
 
+        public static string Join(List<string> segments) {
+            return Join(segments.ToArray());
+        }
+
+        public static string Join(params string[] segments) {
+            return string.Join(KeySeparatorAsString, segments);
+        }
+
         public static string Encode(string key, string fragment) {
             if (string.IsNullOrEmpty(key)) {
                 return fragment;
+            } else if (string.IsNullOrEmpty(fragment)) {
+                return key;
             } else {
                 return string.Format("{0}{1}{2}", key, KeySeparator, fragment);
             }
