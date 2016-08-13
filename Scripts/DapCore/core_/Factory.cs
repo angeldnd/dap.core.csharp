da0313a6355f9c3bddf6d4879f4dea30b622142e
YJ Park <yjpark@gmail.com>
2016-08-13 22:48:19 +0800

    some helper function

--- Scripts/DapCore/core_/Factory.cs
+++ Scripts/DapCore/core_/Factory.cs
@@ -70,5 +70,33 @@ namespace angeldnd.dap {
         public static T Create<T>(params object[] values) {
             return Create<T>(typeof(T), values);
         }
+
+        public static object[] InsertParams(object[] originalValues, params object[] toInsertValues) {
+            if (originalValues == null || originalValues.Length == 0) return toInsertValues;
+            if (toInsertValues == null || toInsertValues.Length == 0) return originalValues;
+
+            object[] values = new object[toInsertValues.Length + originalValues.Length];
+            for (int i = 0; i < toInsertValues.Length; i++) {
+                values[i] = toInsertValues[i];
+            }
+            for (int i = 0; i < originalValues.Length; i++) {
+                values[i + toInsertValues.Length] = originalValues[i];
+            }
+            return values;
+        }
+
+        public static object[] AppendParams(object[] originalValues, params object[] toAppendValues) {
+            if (originalValues == null || originalValues.Length == 0) return toAppendValues;
+            if (toAppendValues == null || toAppendValues.Length == 0) return originalValues;
+
+            object[] values = new object[originalValues.Length + toAppendValues.Length];
+            for (int i = 0; i < originalValues.Length; i++) {
+                values[i] = originalValues[i];
+            }
+            for (int i = 0; i < toAppendValues.Length; i++) {
+                values[i + originalValues.Length] = toAppendValues[i];
+            }
+            return values;
+        }
     }
 }
